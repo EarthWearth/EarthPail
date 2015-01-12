b9240e29bf767031cabad9b53abaf52c251c5599
ron-wolf <19797abe@opayq.com>
2015-01-12 10:16:14 -0500

    squee!
    it compiles, and that’s all that matters

--- pilesort/pilesort.ino
+++ pilesort/pilesort.ino
@@ -4,19 +4,19 @@
 Servo servobj;
 
 // pins
-const byte sensor = A0; // load cell
-const int servo[] = {13, 12}; // list of servo pins
-  const bool pwr = 0; // power pin: always on
-  const bool ctrl = 1; // control pin
+/*const*/ byte sensor = A0; // load cell
+/*const*/ int servo_pin[] = {13, 12}; // list of servo pins
+  /*const*/ bool pwr = 0; // power pin: always on
+  /*const*/ bool ctrl = 1; // control pin
 
 // constants
-const int angle[] = {90, 0, 180}; // list of standard angles
-  const bool flat = 0; // angle when platform is flat
-  const bool trash = 1; // angle when sorting trash
-  const byte recycle = 2; // angle when sorting recycling
-const int delays[] = {80, 5000}; // list of standard delays
-  const bool runs = 0; // delay between loops
-  const bool servo = 1; // delay to allow servo to move into position
+/*const*/ int angle[] = {90, 0, 180}; // list of standard angles
+  /*const*/ bool flat = 0; // angle when platform is flat
+  /*const*/ bool trash = 1; // angle when sorting trash
+  /*const*/ byte recycle = 2; // angle when sorting recycling
+/*const*/ int delays[] = {80, 5000}; // list of standard delays
+  /*const*/ bool runs = 0; // delay between loops
+  /*const*/ bool rot = 1; // delay to allow servo to move into position
 
 // variables
 float zero = 0.0; // weight of platform to subtract from measurements
@@ -27,10 +27,10 @@ int errors; // number of times type of material could not be determined
 
 void setup() {
   pinMode(sensor, INPUT); // initialize load cell
-  pinMode(servo[pwr], OUTPUT); // initialize servo power pin
-    digitalWrite(servo[pwr], HIGH); // power servo
-  pinMode(servo[ctrl], OUTPUT); // initialize servo control pin
-  servobj.attach(servo[ctrl]); // associate servo object with servo control pin
+  pinMode(servo_pin[pwr], OUTPUT); // initialize servo power pin
+    digitalWrite(servo_pin[pwr], HIGH); // power servo
+  pinMode(servo_pin[ctrl], OUTPUT); // initialize servo control pin
+  servobj.attach(servo_pin[ctrl]); // associate servo object with servo control pin
 }
 
 void loop() {
@@ -40,19 +40,19 @@ void loop() {
   }
   if(weight - zero < threshold - error) { // once while loop is interrupted, if plastic with 100% certainty
     servobj.write(angle[recycle]); // sort recycling
-    delay(delays[servo]); // allow servo to move into position
+    delay(delays[rot]); // allow servo to rotate into position
     servobj.write(angle[flat]); // return platform to flat position
     delay(delays[runs]);
   }
   else if(weight - zero > threshold + error) { // if nonrecyclable with 100% certainty
     servobj.write(angle[trash]); // sort trash
-    delay(delays[servo]);
+    delay(delays[rot]);
     servobj.write(angle[flat]);
     delay(delays[runs]);
   }
   else { // if uncertain
     servobj.write(angle[trash]); // sort trash
-    delay(delays[servo]);
+    delay(delays[rot]);
     servobj.write(angle[flat]);
     delay(delays[runs]);
     errors++;
